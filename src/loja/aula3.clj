b66c954bbda95da274c50e19fb228a1f741b2aa7
Matheus Viana <matheus_o_viana@hotmail.com>
2020-06-21 14:16:34 -0300

    feat(aula3.clj): added conclusion of class 3

--- src/loja/aula3.clj
+++ src/loja/aula3.clj
@@ -25,3 +25,78 @@
 
 (println "vals, count and map" (map count
                                (vals (group-by :usuario (l.db/todos-os-pedidos)))))
+
+; THREAD LAST
+
+(->> (l.db/todos-os-pedidos)
+     (group-by :usuario)
+     vals
+     (map count)
+     println)
+
+(defn conta-total-por-usuario
+  [[usuario pedidos]]
+  (println "Usuário" usuario "Pedidos" pedidos)
+  (count pedidos))
+
+(->> (l.db/todos-os-pedidos)
+     (group-by :usuario)
+     (map conta-total-por-usuario)
+     println)
+
+; Usando vetores não é recomendado pq fica uma contagem estranha
+
+(defn conta-total-por-usuario
+  [[usuario pedidos]]
+  (println "Usuário" usuario "Pedidos" pedidos)
+  [usuario (count pedidos)])
+3
+
+(->> (l.db/todos-os-pedidos)
+     (group-by :usuario)
+     (map conta-total-por-usuario)
+     println)
+
+; Usando mapas para retornar pois ficar mais elegante e mais legivel
+
+(defn conta-total-por-usuario
+  [[usuario pedidos]]
+  (println "Usuário" usuario "Pedidos" pedidos)
+  {:usuario-id usuario
+   :total-de-pedidos (count pedidos)})
+
+(->> (l.db/todos-os-pedidos)
+     (group-by :usuario)
+     (map conta-total-por-usuario)
+     println)
+
+(println "PEDIDOS")
+
+(defn total-do-item
+  [[_ detalhes]]
+  (* (get detalhes :quantidade 0) (get detalhes :preco-unitario 0)))
+
+(defn total-do-pedido
+  [pedido]
+  (->> pedido
+    (map total-do-item)
+    (reduce +)))
+
+(defn total-dos-pedidos
+  [pedidos]
+  (->> pedidos
+       (map :itens)
+       (map total-do-pedido)
+       (reduce +)))
+
+(defn quantia-de-pedidos-e-gasto-total-por-usuario
+  [[usuario pedidos]]
+  (println "Usuário >>" usuario "Pedidos >>" pedidos)
+  {:usuario-id usuario
+   :total-de-pedidos (count pedidos)
+   :preco-total (total-dos-pedidos pedidos)})
+
+(->> (l.db/todos-os-pedidos)
+     (group-by :usuario)
+     (map quantia-de-pedidos-e-gasto-total-por-usuario)
+     println)
